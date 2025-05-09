1c23395c0b386588ec71e2030cfa6e1ad7a0b266
chibrik0208 <ilya.ismailau@gmail.com>
2025-05-09 19:28:09 +0200

    added validates

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,6 @@
 class Expense < ApplicationRecord
+  validates :title, :value, :spent_on, presence: true
+  validates :title, length: { maximum: 16 }
+  validates :value, numericality: { greater_than: 0 }
+  validates :title, uniqueness: true 
 end
