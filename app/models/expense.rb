756be0d29c7f44ebb14198e08aeb53079de3612a
chibrik0208 <ilya.ismailau@gmail.com>
2025-05-09 19:50:54 +0200

    added private validate

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,4 +3,12 @@ class Expense < ApplicationRecord
   validates :title, length: { maximum: 16 }
   validates :value, numericality: { greater_than: 0 }
   validates :title, uniqueness: true 
+  validate :spent_on_not_in_future
+
+  private
+
+  def spent_on_not_in_future
+    return if spent_on.blank?
+    errors.add(:spent_on, "cant be in the future") if spent_on > Date.today
+  end
 end
